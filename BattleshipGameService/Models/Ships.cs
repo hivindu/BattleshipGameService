a8d5683e04418b66b8ab863244d61614e9640ddc
hivindu <h.amaradeva@gmail.com>
2021-06-04 13:26:42 +0530

    Adding Ships model

---
+++ BattleshipGameService/Models/Ships.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleshipGameService.Models
+{
+    public class Ships
+    {
+        private string[] battleShip = new string[5];
+        private string[] destroyers = new string[4];
+    }
+}
