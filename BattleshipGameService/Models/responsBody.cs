3ce92164e1e1ba83da629123f4febe7376b10bcc
hivindu <h.amaradeva@gmail.com>
2021-06-05 03:28:53 +0530

    Adding new model

---
+++ BattleshipGameService/Models/responsBody.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleshipGameService.Models
+{
+    public class ResponsBody
+    {
+        public int SelectedValue { get; set; }
+        public bool Hit { get; set; }
+    }
+}
